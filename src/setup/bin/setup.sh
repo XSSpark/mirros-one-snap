15b177de9cfb3999a68bb013ab14b8d21a0c3bc4
Tobias Grasse <tg@glancr.de>
2018-10-06 10:47:46 +0200

    Reuse manual-install script for setup command

--- src/setup/bin/setup.sh
+++ src/setup/bin/setup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 # set -e
 
-# Run any commands needed to setup the snap.
-# This could be downloading a list of dynamic configuration options or creating a database.
-cd $SNAP/api
-
-# Generates an individual secret for each snap installation. Environment variables set here might not be present at all times across revisions, so we store it in a file.
-$SNAP/bin/rails secret > $SNAP_DATA/secret
-cat $SNAP_DATA/secret
-
-SECRET_KEY_BASE=$(cat $SNAP_DATA/secret) RAILS_ENV=production $SNAP/bin/rails db:create db:migrate db:seed
-echo "done with setup"
+# AP setup
+nmcli c add type wifi ifname '*' con-name glancrsetup autoconnect no ssid "glancr setup" mode ap -- ipv4.method manual ipv4.addresses 192.168.8.1 ipv4.dns 192.168.8.1 ipv4.gateway 192.168.8.1
