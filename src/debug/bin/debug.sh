5d0e2be76a3f9b5a2a20ca305d0c5b6ee784b7e8
Tobias Grasse <tg@glancr.de>
2018-10-22 12:47:47 +0200

    Repurpose debug command for backend debugging
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/debug/bin/debug.sh
+++ src/debug/bin/debug.sh
@@ -1,17 +1,5 @@
-echo "Commencing testing binary locations with 'which' (no output indicates that it is not in PATH"
-echo "Testing ruby"
-which ruby
-echo "Testing rails"
-which rails
-echo "Testing gem"
-which gem
-echo "Testing bundler"
-which bundle
+cd $SNAP_DATA/debug/api
 
-echo "Testing sqlite3"
-which sqlite3
+export SECRET_KEY_BASE=$(cat $SNAP_DATA/secret)
+bin/rails server -e production -b 127.0.0.1
 
-echo "Testing ruby with env"
-/usr/bin/env ruby --version
-
-# $SNAP/bin/rails secret
