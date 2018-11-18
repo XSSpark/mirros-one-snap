7fcdec7eccea038c8d87fa8d8128cbb69ff55ca2
Tobias Grasse <tg@glancr.de>
2018-11-18 13:22:17 +0100

    Change to rails app directory before executing cmd
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/setup/bin/setup.sh
+++ src/setup/bin/setup.sh
@@ -4,4 +4,5 @@
 # AP setup
 nmcli c add type wifi ifname '*' con-name glancrsetup autoconnect no ssid "glancr setup" mode ap -- ipv4.method manual ipv4.addresses 192.168.8.1 ipv4.dns 192.168.8.1 ipv4.gateway 192.168.8.1
 
-SECRET_KEY_BASE=$(cat $SNAP_DATA/secret) RAILS_ENV=production $SNAP/bin/bundle exec $SNAP/api/bin/rails runner "Openweathermap::Engine.load_seed" 
+cd $SNAP/api
+SECRET_KEY_BASE=$(cat $SNAP_DATA/secret) RAILS_ENV=production $SNAP/bin/bundle exec rails runner "Openweathermap::Engine.load_seed" 
