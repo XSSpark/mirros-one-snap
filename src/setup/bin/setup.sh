531b987c195c525425bc7888afabd23311a326c2
Tobias Grasse <tg@glancr.de>
2019-05-06 11:50:57 +0200

    Add named LAN connection glancrlan
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/setup/bin/setup.sh
+++ src/setup/bin/setup.sh
@@ -9,6 +9,9 @@
 # AP setup
 nmcli c add type wifi ifname '*' con-name glancrsetup autoconnect no ssid "glancr setup" mode ap -- ipv4.method manual ipv4.addresses 192.168.8.1 ipv4.dns 192.168.8.1 ipv4.gateway 192.168.8.1
 
+# Persistent and predictable LAN connection name
+nmcli c add type ethernet ifname '*' con-name glancrlan autoconnect yes
+
 nmcli d disconnect eth0
 
 # echo "Stopping backend for OWM cities seed"
