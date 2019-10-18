9b94060846cf25a1ab9cff76978624ab9d9ca774
Tobias Grasse <tg@glancr.de>
2019-10-18 11:15:22 +0200

    Refactor NetworkManager calls in hooks to D-Bus
    Leverages the Rails backend implementation to create or tear down the network connections.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/setup/bin/setup.sh
+++ src/setup/bin/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-# AP setup
-nmcli c add type wifi ifname '*' con-name glancrsetup autoconnect no ssid "glancr setup" mode ap -- ipv4.method manual ipv4.addresses 192.168.8.1 ipv4.dns 192.168.8.1 ipv4.gateway 192.168.8.1
-
-# Persistent and predictable LAN connection name
-nmcli c add type ethernet ifname '*' con-name glancrlan autoconnect yes
+# AP + persistent and predictable LAN connection name
+cd "$SNAP/api"
+"$SNAP/wrapper" "$SNAP/api/bin/rails" mirros:setup:network_connections
