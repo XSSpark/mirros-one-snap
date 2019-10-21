759d0c9a77cf9212db0f806d34fa18ebc0b5d59b
Tobias Grasse <tg@glancr.de>
2019-10-21 14:31:24 +0200

    Fix network handling with NmNetwork model
    Setup didn’t include secret variable. Backend now checks for properly synced network connections on each refresh, so we don’t need fragile single jobs in version guards.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/setup/bin/setup.sh
+++ src/setup/bin/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
 # AP + persistent and predictable LAN connection name
+SECRET_KEY_BASE=$(cat "$SNAP_DATA"/secret)
+export SECRET_KEY_BASE
 export RAILS_ENV=production
 cd "$SNAP/api"
 "$SNAP/wrapper" "$SNAP/api/bin/rails" mirros:setup:network_connections
